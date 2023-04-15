f1842d61aa168f9fde91502aab7e5eafaeb64c5c
Dante Dantas <dantedantasloc@gmail.com>
2023-04-15 01:08:10 -0400

    primeira versão do website

---
+++ assets/script.js
@@ -0,0 +1,168 @@
+confirm("Are you sure you want to delete?")
+$(document).ready(function(){
+
+    $(".mudaTela").click(function(){
+        mudaTela( $(this), $(this).attr("nova"), $(this).attr("animacao"), $(this).attr("tempoAnimacao") );
+    });
+
+    $("a.opcoes").click(function(e){
+        e.preventDefault();
+        $("div.opcoes").slideToggle(500);
+    });
+
+    $(".calendario .marcado").click(function(){
+        mostraMsgMes($(this).attr("value"));
+    });
+
+    const mudaTela = ( atual, nova = null, animacao = "fade", tempoAnimacao = 900 ) => {
+
+        // define a nova tela
+        if(!nova){
+            nova = parseInt(atual.parent().attr("id").split("tela")[1])+1;
+        }
+
+        if(animacao == "fade"){
+            $("#tela"+(nova-1)).fadeOut(tempoAnimacao);
+            setTimeout(() => {
+                $("#tela"+nova).fadeIn(tempoAnimacao)
+            }, tempoAnimacao);
+        }else{
+            $("#tela"+(nova-1)).hide(tempoAnimacao);
+            $("#tela"+nova).show(tempoAnimacao);
+        }
+
+        if($("#tela"+nova).hasClass("temporizado")){
+            $("#tela"+nova+" div").hide();
+            telaTemporizada(nova, 0);
+        }
+
+        verificaFundo(nova);
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        if(nova == 5){
+            var audio = new Audio('assets/musica.mp3');
+            audio.volume = 0.1;
+            audio.play();
+        }
+        
+    }
+
+    const telaTemporizada = ( nTela, contador ) =>{
+
+        const tela = $("#tela"+nTela+" div:eq("+contador+")");
+        const temporizador = 500;
+        const temporizadorPrimeiraTela = (contador==0?$("#tela"+nTela).attr("tempo"):temporizador);
+
+        setTimeout(() => {
+            tela.fadeIn(temporizador);
+
+            setTimeout(() => {
+                tela.fadeOut(temporizador);
+                if(tela.attr("final") == "true"){
+                    mudaTela(null, nTela+1, "fade", 900);
+                    verificaFundo(nTela+1);
+                }else{
+                    telaTemporizada(nTela, contador+1);
+                }
+
+            }, tela.attr("tempo") );
+
+        }, temporizadorPrimeiraTela);
+        
+    }
+
+    const verificaFundo = (nTela) =>{
+
+        const fundo = $("#tela"+nTela).attr("fundo");
+        const tempo = $("#tela"+nTela).attr("tempo");
+
+        if(fundo){
+            $("body").attr("class", fundo);            
+        }
+        
+    }
+
+    const mostraMsgMes = (texto) =>{
+        let titulo;
+        let mensagem;
+    
+        switch(texto) {
+            case "30/3": 
+                titulo = "30 de Março de 2023"; 
+                mensagem = "<p>Esse foi o dia que nos conhecemos! Ou pelo menos, o dia que nos conhecemos já sabendo que dali pra frente poderíamos ter alguma coisa juntos. Conhecemos mais aleatório possível pqp e eu jurava que nem daqui d emanaus você era!</p>";
+                break;
+            case "8/2": 
+                titulo = null; 
+                mensagem = null;
+                break;
+            case "14/1": 
+                titulo = null; 
+                mensagem = null;
+                break;
+            case "3/2": 
+                titulo = null; 
+                mensagem = null;
+                break;
+            case "5/2": 
+                titulo = null; 
+                mensagem = null;
+                break;
+            case "10/4": 
+                titulo = null; 
+                mensagem = null;
+                break;
+            case "11/4": 
+                titulo = null; 
+                mensagem = null;
+                break;
+            case "12/4": 
+                titulo = "12 de abril de 2023"; 
+                mensagem = "<section class='text-center'><p class='letra-vermelha'><strong>Este momento está sendo escrito agora...</strong></p></section>";
+                break;
+            case "15/4": 
+                titulo = "15 de abril de 2023"; 
+                mensagem = "<section class='text-center mt-5 mb-5'><p><strong>O dia em que ela disse<br><span class='letra2 letra-vermelha'>SIM</span></strong></p></section>";
+                break;
+            default:
+                titulo = null; 
+                mensagem = null;
+                break;
+        }
+    
+        if (texto === "final") {
+            titulo = "15 de abril de 2023"; 
+            mensagem = "<section class='text-center mt-5 mb-5'><p><strong>O dia em que ela disse<br><span class='letra2 letra-vermelha'>SIM</span></strong></p></section>";
+            mostraPopUp(true, titulo, mensagem);
+            telaFinal = true;
+        }
+    };
+    
+    
+
+});
+
+let telaFinal = false;
+
+const mostraPopUp = (mostrar, titulo = "Título de testes", mensagem = "Mensagem de teste...") =>{
+
+    if(mostrar){
+        $("html, body").animate({ scrollTop: $(".pop-up")[0].offsetTop }, "smooth");
+        $(".pop-up").fadeIn(500);
+        $(".pop-up h1").html(titulo);
+        $(".pop-up div").html(mensagem);
+        $(".container").css("opacity", "0.5");
+    }else{
+        $(".pop-up").fadeOut(500);
+        $(".container").css("opacity", "1");
+
+        if(telaFinal){
+            $("#tela19").fadeOut(4000);
+            setTimeout(() => {
+                $("#tela20").fadeIn(6500);
+                $("body").attr("class", "fundo6");    
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+            }, 4000);
+        }
+
+    }
+
+}
